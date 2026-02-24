e28986bbe6ea4b29cc989cd9845355499309c556
Dursax <120788048+Dursax@users.noreply.github.com>
2026-02-24 14:49:56 +0100

    Actualizacion automatica y generacion de enlaces

--- Actualizar_Nube.bat
+++ Actualizar_Nube.bat
@@ -4,18 +4,37 @@ echo   Subiendo cambios a GitHub, Maestro...
 echo ==========================================
 cd /d "%~dp0"
 
+:: 1. Subir los archivos a GitHub
 git add .
-git commit -m "Actualizacion automatica: %date% %time%"
+git commit -m "Actualizacion automatica y generacion de enlaces"
 git push
 
 echo.
 echo ==========================================
-echo   ¡Listo! Tus actividades ya estan en internet.
+echo   ¡Listo! Nube actualizada.
+echo   Generando tu lista de enlaces...
 echo ==========================================
+
+:: 2. Crear el archivo de texto con los enlaces
+set "archivo=Enlaces_Google_Sites.txt"
+
+echo ========================================== > "%archivo%"
+echo MIS ENLACES DE EXELEARNING PARA GOOGLE SITES >> "%archivo%"
+echo ========================================== >> "%archivo%"
+echo. >> "%archivo%"
+
+:: 3. Leer cada carpeta y generar su enlace exacto
+for /d %%D in (*) do (
+    :: Ignorar carpetas del sistema (.git)
+    if /I not "%%D"==".git" (
+        echo Actividad: %%D >> "%archivo%"
+        echo Enlace: https://dursax.github.io/Music-EducacionPrimaria-Enlaces/%%D/ >> "%archivo%"
+        echo -------------------------------------------------- >> "%archivo%"
+    )
+)
+
 echo.
-echo Tu enlace base para poner en Google Sites es:
-echo https://dursax.github.io/Music-EducacionPrimaria-Enlaces/
-echo.
-echo Recuerda: Solo tienes que anadir el nombre de la carpeta al final del enlace.
+echo ¡Perfecto! Se ha creado un archivo llamado "%archivo%"
+echo Abrelo para copiar y pegar tus enlaces.
 echo.
 pause
